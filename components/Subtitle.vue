6e00093ec5770e2c70198645c522cbad10c5056e
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-03-11 19:47:03 -0300

    add subtitle component
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ components/Subtitle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col border p-2">
+    <h1>Subtitle:</h1>
+    <div class="flex">
+      <div
+        v-for="subtitle in items"
+        :key="subtitle.name"
+        class="flex flex-row items-center mr-5"
+      >
+        <img :src="subtitle.image" :alt="subtitle.name" />
+        <p class="ml-2">{{ subtitle.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => [],
+      validator: (value) =>
+        value.reduce((acc, val) => acc && val.name && val.image, true),
+    },
+  },
+}
+</script>
+
+<style></style>
