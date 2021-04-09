caf970f0e1623ffcea17010d3ca5075996367f18
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-04-09 19:21:57 -0300

    create one service page
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ components/Table.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="m-5">
+    <h2 class="text-lg font-bold">{{ title }}</h2>
+    <table class="table-auto">
+      <thead>
+        <tr>
+          <th v-for="column in columns" :key="column">{{ column }}</th>
+        </tr>
+      </thead>
+      <tbody class="border border-black">
+        <tr v-for="c in numberOfColumns" :key="c" class="border border-black">
+          <td
+            v-for="value in data[c - 1]"
+            :key="value"
+            class="text-center border border-black"
+          >
+            {{ value }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: () => '',
+    },
+    columns: {
+      type: Array,
+      required: true,
+      default: () => [],
+      validator: (columns) =>
+        columns.every((column) => typeof column === 'string'),
+    },
+    data: {
+      type: Array,
+      required: true,
+      default: () => [],
+      validator: (data) => data.every((d) => Array.isArray(d)),
+    },
+  },
+  computed: {
+    numberOfColumns() {
+      return this.columns.length
+    },
+  },
+}
+</script>
+
+<style></style>
