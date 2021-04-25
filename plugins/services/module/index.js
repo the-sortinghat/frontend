b8ae4d63b51fe4699a131bc92b30e808fa788a08
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-04-25 02:01:28 -0300

    adapt module data service to obtain info from more than one route
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- plugins/services/module/index.js
+++ plugins/services/module/index.js
@@ -1,15 +1,28 @@
 /* eslint-disable no-console */
 function parseData(data) {
   // parsing data into a valid object
-  return data
+  const { module, modServices, metrics } = data
+  const { name, responsibility } = module
+  const services = modServices.services.map(({ id, name }) => ({ id, name }))
+  const links = modServices.links
+
+  return {
+    name,
+    responsibility,
+    services,
+    links,
+    metrics,
+  }
 }
 
 async function getModuleData(moduleId, $axios) {
-  let moduleData = []
+  let moduleData = {}
 
   try {
-    const response = await $axios.$get(`/api/modules/${moduleId}`)
-    moduleData = parseData(response)
+    const module = await $axios.$get(`/api/modules/${moduleId}`)
+    const modServices = await $axios.$get(`/api/modules/${moduleId}/services`)
+    const metrics = await $axios.$get(`/api/modules/${moduleId}/metrics`)
+    moduleData = parseData({ module, modServices, metrics })
   } catch (err) {
     console.log('error occuried while fetching from api...')
     console.log(err.message)
--- plugins/services/module/module.spec.js
+++ plugins/services/module/module.spec.js
@@ -1,32 +1,50 @@
 import getModuleData from './index'
 
-describe('getModuleData function', () => {
+describe('getModuleData()', () => {
   let moduleData
 
   beforeAll(async () => {
     moduleData = await getModuleData('1', {
-      $get: jest.fn((_) =>
-        Promise.resolve({
-          name: undefined,
-          responsibility: undefined,
-          services: [],
-          metrics: [],
-        })
-      ),
+      $get: jest
+        .fn()
+        .mockReturnValueOnce(
+          Promise.resolve({
+            name: undefined,
+            responsibility: undefined,
+          })
+        )
+        .mockReturnValueOnce(
+          Promise.resolve({
+            services: [
+              { id: 1, name: '' },
+              { id: 2, name: '' },
+            ],
+            links: [{ source: 1, target: 2, type: 'async' }],
+          })
+        )
+        .mockReturnValueOnce(
+          Promise.resolve([
+            { metric: '', measure: { min: 0, max: 1, value: 0 } },
+          ])
+        ),
     })
   })
 
-  it('returns a module data with essential properties', () => {
-    const keys = ['name', 'responsibility', 'services', 'metrics']
+  it('returns module data with essential properties', () => {
+    const keys = ['name', 'responsibility', 'services', 'links', 'metrics']
 
     expect(Object.keys(moduleData).sort()).toMatchObject(keys.sort())
   })
 
-  it("returns an array of modules' services", () => {
+  it("returns an array of module's services", () => {
     expect(moduleData.services).toBeInstanceOf(Array)
   })
 
-  it("returns an array of modules' metrics", () => {
+  it("returns an array of module's links", () => {
+    expect(moduleData.links).toBeInstanceOf(Array)
+  })
+
+  it("returns an array of module's metrics", () => {
     expect(moduleData.metrics).toBeInstanceOf(Array)
   })
 
@@ -38,6 +56,19 @@ describe('getModuleData function', () => {
           name: expect.any(String),
         })
       )
+    })
+  })
+
+  it('returns data with essential properties for each link', () => {
+    moduleData.links.forEach((link) => {
+      expect(link).toEqual(
+        expect.objectContaining({
+          source: expect.any(Number),
+          target: expect.any(Number),
+          type: expect.any(String),
+        })
+      )
+      expect(['sync', 'async']).toContain(link.type)
     })
   })
 
